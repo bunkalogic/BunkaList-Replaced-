6c3548a3df26c1fd9b6d2cda2413265fbdf2271a
Steinspass <37332713+Steinspass@users.noreply.github.com>
2019-02-18 17:59:36 +0100

    Created LoginActivity, SignUpActivity and ForgotPassword

---
+++ app/src/main/java/com/bunkalogic/bunkalist/Activities/LoginActivities/LoginActivity.kt
@@ -0,0 +1,13 @@
+package com.bunkalogic.bunkalist.Activities.LoginActivities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.bunkalogic.bunkalist.R
+
+class LoginActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
--- app/src/main/java/com/bunkalogic/bunkalist/ForgotPasswordActivity.kt
+++ app/src/main/java/com/bunkalogic/bunkalist/ForgotPasswordActivity.kt
@@ -3,10 +3,10 @@ package com.bunkalogic.bunkalist
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class ForgotPasswordActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_forgot_password)
     }
 }
