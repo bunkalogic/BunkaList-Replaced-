65082ee4f63c74a75ad48a5857785cc5c24704f8
Steinspass <37332713+Steinspass@users.noreply.github.com>
2019-02-19 14:35:33 +0100

    created The SignUpActivity Layout

--- app/src/main/java/com/bunkalogic/bunkalist/Activities/LoginActivities/LoginActivity.kt
+++ app/src/main/java/com/bunkalogic/bunkalist/Activities/LoginActivities/LoginActivity.kt
@@ -3,11 +3,14 @@ package com.bunkalogic.bunkalist.Activities.LoginActivities
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import com.bunkalogic.bunkalist.R
+import kotlinx.android.synthetic.main.activity_login.*
+import org.jetbrains.anko.intentFor
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+        buttonCreateAccount.setOnClickListener { startActivity(intentFor<SignUpActivity>()) }
     }
 }
