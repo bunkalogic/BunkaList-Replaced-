f7d4c77b8bfb32a8b2e3d529f4c1adf7173844b2
Steinspass <37332713+Steinspass@users.noreply.github.com>
2019-02-25 20:46:03 +0100

    Add button Sign Out in SettingsFragment

--- app/src/main/java/com/bunkalogic/bunkalist/Fragments/SettingsFragment.kt
+++ app/src/main/java/com/bunkalogic/bunkalist/Fragments/SettingsFragment.kt
@@ -1,22 +1,38 @@
 package com.bunkalogic.bunkalist.Fragments
 
-import android.content.Context
-import android.net.Uri
+
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bunkalogic.bunkalist.Activities.Login.LoginActivity
 
 import com.bunkalogic.bunkalist.R
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.fragment_settings.*
+import kotlinx.android.synthetic.main.fragment_settings.view.*
+import org.jetbrains.anko.clearTask
+import org.jetbrains.anko.newTask
+import org.jetbrains.anko.support.v4.intentFor
 
 class SettingsFragment : Fragment() {
 
+    private lateinit var _view: View
+
+    private val mAuth: FirebaseAuth by lazy { FirebaseAuth.getInstance() }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_settings, container, false)
+        _view =  inflater.inflate(R.layout.fragment_settings, container, false)
+
+        _view.buttonSignOut.setOnClickListener {
+            mAuth.signOut()
+            startActivity(intentFor<LoginActivity>().newTask().clearTask())
+        }
+
+        return _view
     }
 
 
