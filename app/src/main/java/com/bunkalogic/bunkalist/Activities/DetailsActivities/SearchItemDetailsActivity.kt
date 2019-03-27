e2ade5b883efbc7dad9498150beed2c0551dfa93
Steinspass <37332713+Steinspass@users.noreply.github.com>
2019-03-27 19:52:52 +0100

    Add layout SearchItemDetails

---
+++ app/src/main/java/com/bunkalogic/bunkalist/Activities/DetailsActivities/SearchItemDetailsActivity.kt
@@ -0,0 +1,13 @@
+package com.bunkalogic.bunkalist.Activities.DetailsActivities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.bunkalogic.bunkalist.R
+
+class SearchItemDetailsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        setContentView(R.layout.search_item_full_details)
+        super.onCreate(savedInstanceState)
+    }
+}
